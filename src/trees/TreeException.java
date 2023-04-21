46f21071992c348122cb5ccc4d27d8a4e92027ee
Miko Tonthat <tonthatmiko@gmail.com>
2023-04-21 09:07:50 -0700

    updated TreeNode class

--- src/trees/TreeException.java
+++ src/trees/TreeException.java
@@ -8,3 +8,18 @@ public class TreeException extends Exception
 		super(s);
 	}
 }
+
+//5: On branch master
+//Changes not staged for commit:
+	//  (use "git add <file>..." to update what will be committed)
+	  //(use "git restore <file>..." to discard changes in working directory)
+	    //    modified:   src/trees/FamilyTree.java
+	      //  modified:   src/trees/TreeException.java
+	//Untracked files:
+	  //(use "git add <file>..." to include in what will be committed)
+	    //    .classpath
+	      //  .project
+	        //.settings/
+	        //bin/
+
+//6: i see a folder called src/trees and a file called .gitignore
